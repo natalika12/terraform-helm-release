6a267be21826ce68596220a5da97324643e243ab
natalika12 <nataliiarigoni@gmail.com>
2024-08-29 17:53:33 -0500

    fixed

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "helm_release" "wordpress" {
-  name             = var.name
-  chart            = "${path.module}/myapp"
-  namespace        = "wordpress"
-  wait             = false
-  depends_on       = [kubernetes_namespace.wordpress]
-  values           = var.values
+resource "helm_release" "wordpress2" {
+  name       = var.name
+  chart      = "${path.module}/myapp"
+  namespace  = "wordpress"
+  wait       = false
+  depends_on = [kubernetes_namespace.wordpress]
+  values     = var.values
 }
